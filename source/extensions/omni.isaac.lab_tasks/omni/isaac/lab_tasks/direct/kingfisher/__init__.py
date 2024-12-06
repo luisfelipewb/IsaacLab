7add69238a02cfb22a34683402d3905b2596c74e
Luis Felipe Wolf Batista <luis.batista@gatech.edu>
2024-12-06 15:23:49 +0100

    Add base task using the kingfisher
    Based on the quadcopter task, but using the kingfisher with
    hydrodynamics and hydrostatics.

---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/kingfisher/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Quacopter environment.
+"""
+
+import gymnasium as gym
+
+from . import agents
+from .kingfisher_env import KingfisherEnv, KingfisherEnvCfg
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Kingfisher-Direct-v0",
+    entry_point="omni.isaac.lab_tasks.direct.kingfisher:KingfisherEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": KingfisherEnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
